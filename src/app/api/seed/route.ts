1902cc74c6a3671cd8d22d014282ea65b8c46f03
Alexander <javc1603@gmail.com>
2024-04-17 01:47:24 -0600

    solving issues with deploy

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,29 +1,29 @@
-import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server'
-import bcrypt from 'bcryptjs';
+// import prisma from '@/lib/prisma';
+// import { NextResponse, NextRequest } from 'next/server'
+// import bcrypt from 'bcryptjs';
 
-export async function GET(request: Request) { 
+// export async function GET(request: Request) { 
 
-  await prisma.todo.deleteMany(); // delete * from todo
-  await prisma.user.deleteMany(); // delete * from todo
+//   await prisma.todo.deleteMany(); // delete * from todo
+//   await prisma.user.deleteMany(); // delete * from todo
 
-  const user = await prisma.user.create({
-    data: {
-      email: 'test1@google.com',
-      password: bcrypt.hashSync('123456'),
-      roles: ['admin','client','super-user'],
-      todos: {
-        create: [
-          { description: 'todo 1', complete: true },
-          { description: 'todo 2' },
-          { description: 'todo 3' },
-          { description: 'todo 4' },
-          { description: 'todo 5' },
-        ]
-      }
-    }
-  });
+//   const user = await prisma.user.create({
+//     data: {
+//       email: 'test1@google.com',
+//       password: bcrypt.hashSync('123456'),
+//       roles: ['admin','client','super-user'],
+//       todos: {
+//         create: [
+//           { description: 'todo 1', complete: true },
+//           { description: 'todo 2' },
+//           { description: 'todo 3' },
+//           { description: 'todo 4' },
+//           { description: 'todo 5' },
+//         ]
+//       }
+//     }
+//   });
   
 
-  return NextResponse.json({ message: 'Seed Executed' });
-}
+//   return NextResponse.json({ message: 'Seed Executed' });
+// }
